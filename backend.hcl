400c6909ab368b85e51f47be70446ba22bc4d6d0
ignatiusm <ignatius.menzies@gmail.com>
2023-08-23 12:10:17 +1200

    fix(actions): create bucket first time

--- backend.hcl
+++ backend.hcl
@@ -1,6 +1,6 @@
 # Shared info for remote terraform state for personal tf projects
-bucket         = "ignatiusm-terraform-state-files"
+bucket         = "ignatiusm-tf-state-files"
 region         = "ap-southeast-2"
 
-dynamodb_table = "ignatiusm-terraform-locks"
+dynamodb_table = "ignatiusm-tf-locks"
 encrypt        = true
--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "ignatiusm-terraform-state-files"
+  bucket = "ignatiusm-tf-state-files"
  
   # Prevent accidental deletion of this S3 bucket
   lifecycle {
@@ -37,7 +37,7 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "ignatiusm-terraform-locks"
+  name         = "ignatiusm-tf-locks"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
